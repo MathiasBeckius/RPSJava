9121596a49f4ddad280c25297ee68e01de12abed
Mathias Beckius <mathiasbeckius@hotmail.com>
2019-11-17 22:34:57 +0100

    Removed HandShapeMapping - LetUserChooseHandShapeState is now a bit clearer!

--- src/terminalgame/LetUserChooseHandShapeState.java
+++ src/terminalgame/LetUserChooseHandShapeState.java
@@ -1,43 +1,47 @@
 package terminalgame;
 
+import java.util.Hashtable;
 import rockpaperscissors.Game;
 import rockpaperscissors.Tally;
 import rockpaperscissors.HandShape;
+import rockpaperscissors.HandShapes;
 import terminalgame.ui.TerminalUI;
 
 class LetUserChooseHandShapeState extends State
 {
     private TerminalUI ui;
     private Tally tally;
-    private HandShapeMapping mapping;
 
     public LetUserChooseHandShapeState(TerminalUI ui, Tally tally)
     {
         this.ui = ui;
         this.tally = tally;
-        this.mapping = new HandShapeMapping("r", "p", "s");
     }
 
     public State run()
     {
-        HandShape playersHand;
-        String userInput = this.ui.readLine(this.message());
-        userInput = userInput.toLowerCase();
-        try
-        {
-            playersHand = this.mapping.mapToShape(userInput);
-        }
-        catch (IllegalArgumentException e)
-        {
+        HandShape playersHand = this.letUserChooseShape();
+        if (playersHand == null)
             return this;
-        }
         HandShape opponentsHand = RandomShape.shape();
         this.tally = Game.updatedTally(this.tally, playersHand, opponentsHand);
         return new PresentResultState(this.ui, this.tally);
     }
 
-    private String message()
+    private HandShape letUserChooseShape()
     {
-        return "[R]ock, [P]aper or [S]cissors? ";
+        String message = "[R]ock, [P]aper or [S]cissors? ";
+        String userInput = this.ui.readLine(message);
+        userInput = userInput.toLowerCase();
+        return this.mapTable().get(userInput);
+    }
+
+    private Hashtable<String, HandShape> mapTable()
+    {
+        Hashtable<String, HandShape> table = new Hashtable<String, HandShape>();
+        table.put("r", HandShapes.rock());
+        table.put("p", HandShapes.paper());
+        table.put("s", HandShapes.scissors());
+        return table;
     }
 }
